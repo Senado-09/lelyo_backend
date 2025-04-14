d5a1a48e75fae2c4b85016f7cae2669ef054a197
Sena DOMONHEDO <senadomonhedo@outlook.fr>
2025-04-14 02:45:47 +0100

    final version

--- main.py
+++ main.py
@@ -42,7 +42,10 @@ def root():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=[
+        "http://localhost:5173",
+        "https://lelyo.netlify.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
